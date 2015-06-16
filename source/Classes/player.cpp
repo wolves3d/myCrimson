81516d64ac0acb76625080de5ecc7ab3d2e09c63
Vladimir Kovner <wolves3d@gmail.com>
2015-06-16 19:29:03 +0300

    WASD player moving (very simple)

--- source/Classes/player.cpp
+++ source/Classes/player.cpp
@@ -1,1 +1,62 @@
 #include "pch.h"
+#include "player.h"
+
+
+Player::Player()
+{
+
+}
+
+
+bool Player::init()
+{
+	if (false == CCNode::init())
+		return false;
+
+	CCSprite* pSprite = CCSprite::create("hero.png");
+
+	if (NULL == pSprite)
+	{
+		return false;
+	}
+
+	addChild(pSprite);
+	scheduleUpdate();
+	return true;
+}
+
+
+void Player::update(float delta)
+{
+	HandleKeyboardInput();
+}
+
+
+void Player::HandleKeyboardInput()
+{
+	if (0x80 & GetKeyState('W'))
+	{
+		CCPoint prevPos = getPosition();
+		prevPos.y += 10;
+		setPosition(prevPos);
+	}
+	else if (0x80 & GetKeyState('S'))
+	{
+		CCPoint prevPos = getPosition();
+		prevPos.y -= 10;
+		setPosition(prevPos);
+	}
+
+	if (0x80 & GetKeyState('A'))
+	{
+		CCPoint prevPos = getPosition();
+		prevPos.x -= 10;
+		setPosition(prevPos);
+	}
+	else if (0x80 & GetKeyState('D'))
+	{
+		CCPoint prevPos = getPosition();
+		prevPos.x += 10;
+		setPosition(prevPos);
+	}
+}
--- source/Classes/player.h
+++ source/Classes/player.h
@@ -5,7 +5,13 @@ class Player : public CCNode
 {
 public:
 	Player();
+	CREATE_FUNC(Player);
 
+private:
+	virtual bool init();
+	virtual void update(float delta);
+
+	void HandleKeyboardInput();
 };
 
 #endif // #__player_h_included__
