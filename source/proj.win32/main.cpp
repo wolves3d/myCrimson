f096039b39c27533ce091848ff2d1175430e78b7
Vladimir Kovner <wolves3d@gmail.com>
2015-06-16 19:29:21 +0300

    window size tuning

--- source/proj.win32/main.cpp
+++ source/proj.win32/main.cpp
@@ -17,6 +17,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     eglView->setFrameSize(2048, 1536);
     // The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
     // So we need to invoke 'setFrameZoomFactor'(only valid on desktop(win32, mac, linux)) to make the window smaller.
-    eglView->setFrameZoomFactor(0.4f);
+    eglView->setFrameZoomFactor(0.5f);
     return CCApplication::sharedApplication()->run();
 }
